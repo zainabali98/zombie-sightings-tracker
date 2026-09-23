7d582d718273e98c245f9de345a148cdd06ec974
zainab <zainab_ali_juma@hotmail.com>
2026-09-23 16:15:43 +0300

    feat: add location routes

--- routes/location.routes.js
+++ routes/location.routes.js
@@ -6,9 +6,14 @@ const Location = require('../models/Locations')
 
 
 
+router.get('/', async (req, res)=>{
+ const locations = await Location.find()
+ res.render('locations.ejs', {locations: locations})
+})
 
-
-
-
+router.get('/:id', async (req, res) =>{
+    const foundLocation = await Location.findById(req.params.id)
+    res.render('location-details.ejs', {location: foundLocation})
+})
 
 module.exports = router
